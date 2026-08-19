262415180b5ddaf80a10f6ea1a1ff7860596d478
Harini Suresh <harinis1@uci.edu>
2026-08-18 19:44:39 -0700

    chore: initial repo setup

---
+++ OnTask/OnTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTaskApp.swift
+//  OnTask
+//
+//  Created by Harini Suresh on 8/18/26.
+//
+
+import SwiftUI
+
+@main
+struct OnTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
